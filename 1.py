1aa050f2d50fb206ffb1a7d06e75cc2ba27cc91b
Walther <veeti.haapsamo@gmail.com>
2015-12-07 17:37:07 +0200

    Add second part of puzzle

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
 i = input()
 floor = 0
+instruction = 0
 for x in range(0, len(i)):
     if i[x] == '(':
-        floor +=1;
+        floor +=1
     elif i[x] == ')':
-        floor -=1;
-print(floor)
+        floor -=1
+    if (floor < 0 and instruction == 0):
+        instruction = x+1
+print("floor: %s" % floor)
+print("basement entry: %s" % instruction)
