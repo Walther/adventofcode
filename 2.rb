868fd20d793130cc91241bcb82d63e0baa49aeaf
Walther <veeti.haapsamo@gmail.com>
2015-12-07 17:55:13 +0200

    Add solution to second part of puzzle

--- 2.rb
+++ 2.rb
@@ -1,13 +1,21 @@
-total = 0
+paper = 0
+ribbon = 0;
 $stdin.each_line do |box|
+    # Parse sides
     nums = box.split('x').map(&:to_i)
     l = nums[0]
     w = nums[1]
     h = nums[2]
+    sides = [l,w,h]
+    # Compute faces
     s1 = l*w
     s2 = w*h
     s3 = h*l
-    total += 2*(s1 + s2 + s3) + [s1, s2, s3].min
+    faces = [s1, s2, s3]
+    # Sum
+    paper += 2 * faces.reduce(:+) + faces.min
+    ribbon += 2 * sides.min(2).reduce(:+) + sides.reduce(:*)
 end
 
-print total
+print "paper: ", paper, "\n"
+print "ribbon: ", ribbon, "\n"
